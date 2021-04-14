1d8838622d15d3d718d6b084cf0f0ed9d964a6f3
Priti Kumari <rohit.sharma@celebaltech.com>
2021-04-14 17:10:36 +0530

    initialcode

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace DemoSaveDataInLocalDBUsingQuery
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvOldData = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.brnUpdate = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtQualification = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblId = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOldData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvOldData
+            // 
+            this.dgvOldData.AllowUserToAddRows = false;
+            this.dgvOldData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOldData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dgvOldData.Location = new System.Drawing.Point(17, 236);
+            this.dgvOldData.Name = "dgvOldData";
+            this.dgvOldData.Size = new System.Drawing.Size(450, 201);
+            this.dgvOldData.TabIndex = 20;
+            this.dgvOldData.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvOldData_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Select";
+            this.Column1.Name = "Column1";
+            this.Column1.Text = "Select";
+            this.Column1.UseColumnTextForButtonValue = true;
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "name";
+            this.Column2.HeaderText = "Name";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "Age";
+            this.Column3.HeaderText = "Age";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "Qualification";
+            this.Column4.HeaderText = "Qualification";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.DataPropertyName = "id";
+            this.Column5.HeaderText = "id";
+            this.Column5.Name = "Column5";
+            this.Column5.Visible = false;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Enabled = false;
+            this.btnDelete.Location = new System.Drawing.Point(277, 161);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(73, 23);
+            this.btnDelete.TabIndex = 19;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // brnUpdate
+            // 
+            this.brnUpdate.Enabled = false;
+            this.brnUpdate.Location = new System.Drawing.Point(182, 161);
+            this.brnUpdate.Name = "brnUpdate";
+            this.brnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.brnUpdate.TabIndex = 18;
+            this.brnUpdate.Text = "Update";
+            this.brnUpdate.UseVisualStyleBackColor = true;
+            this.brnUpdate.Click += new System.EventHandler(this.brnUpdate_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(84, 161);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 17;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtQualification
+            // 
+            this.txtQualification.Location = new System.Drawing.Point(163, 107);
+            this.txtQualification.Name = "txtQualification";
+            this.txtQualification.Size = new System.Drawing.Size(196, 20);
+            this.txtQualification.TabIndex = 16;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(163, 68);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(110, 20);
+            this.txtAge.TabIndex = 15;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(163, 26);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(199, 20);
+            this.txtName.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(73, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Qualification:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Age:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Name:";
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(391, 33);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(34, 13);
+            this.lblId.TabIndex = 21;
+            this.lblId.Text = "hdnId";
+            this.lblId.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 462);
+            this.Controls.Add(this.dgvOldData);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.brnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtQualification);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblId);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOldData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvOldData;
+        private System.Windows.Forms.DataGridViewButtonColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button brnUpdate;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtQualification;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblId;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlServerCe;
+using System.IO;
+
+
+namespace DemoSaveDataInLocalDBUsingQuery
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            BindGrid();
+        }
+
+        /// <summary>
+        /// Save record directly in local database table using query.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCeConnection con = new SqlCeConnection("Data Source="
+                    + System.IO.Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location), "MyDB.sdf"));
+                con.Open();
+                SqlCeCommand cmd = con.CreateCommand();
+                cmd.CommandText = "insert into MyDemoTable(name,Age,Qualification) values(@name,@Age,@Qualification)";
+                cmd.Parameters.AddWithValue("@name", txtName.Text.Trim());
+                cmd.Parameters.AddWithValue("@Age", txtAge.Text.Trim());
+                cmd.Parameters.AddWithValue("@Qualification", txtQualification.Text.Trim());
+                cmd.CommandType = CommandType.Text;
+                cmd.ExecuteNonQuery();
+                con.Close();
+                BindGrid();
+                Reset();
+                MessageBox.Show("Record Save Successfully.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Update exsting record using query.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void brnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCeConnection con = new SqlCeConnection("Data Source="
+                   + System.IO.Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location), "MyDB.sdf"));
+                con.Open();
+                SqlCeCommand cmd = con.CreateCommand();
+                cmd.CommandText = "update MyDemoTable set name=@name,Age=@Age,Qualification=@Qualification where id=@id";
+                cmd.Parameters.AddWithValue("@id", lblId.Text);
+                cmd.Parameters.AddWithValue("@name", txtName.Text.Trim());
+                cmd.Parameters.AddWithValue("@Age", txtAge.Text.Trim());
+                cmd.Parameters.AddWithValue("@Qualification", txtQualification.Text.Trim());
+                cmd.CommandType = CommandType.Text;
+                cmd.ExecuteNonQuery();
+                con.Close();
+                BindGrid();
+                Reset();
+                MessageBox.Show("Record Updated Successfully.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Delete record from table using query.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCeConnection con = new SqlCeConnection("Data Source="
+                    + System.IO.Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location), "MyDB.sdf"));
+                con.Open();
+                SqlCeCommand cmd = con.CreateCommand();
+                cmd.CommandText = "delete  MyDemoTable where id=@id";
+                cmd.Parameters.AddWithValue("@id", lblId.Text);
+                cmd.CommandType = CommandType.Text;
+                cmd.ExecuteNonQuery();
+                con.Close();
+                BindGrid();
+                Reset();
+                MessageBox.Show("Record Removed Successfully.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private void dgvOldData_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvOldData.Rows.Count > 0 && e.RowIndex != -1)
+            {
+                if (dgvOldData.Rows[e.RowIndex].Cells[0].Selected)
+                {
+                    txtName.Text = dgvOldData.Rows[e.RowIndex].Cells[1].Value.ToString();
+                    txtAge.Text = dgvOldData.Rows[e.RowIndex].Cells[2].Value.ToString();
+                    txtQualification.Text = dgvOldData.Rows[e.RowIndex].Cells[3].Value.ToString();
+                    lblId.Text = dgvOldData.Rows[e.RowIndex].Cells[4].Value.ToString();
+
+                    btnDelete.Enabled = true;
+                    brnUpdate.Enabled = true;
+                    btnAdd.Enabled = false;
+
+                }
+            }
+        }
+
+        /// <summary>
+        /// bind grid with all data.
+        /// </summary>
+        private void BindGrid()
+        {
+            DataSet _ds = new DataSet();
+            SqlCeConnection con = new SqlCeConnection("Data Source="
+               + System.IO.Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location), "MyDB.sdf"));
+            SqlCeDataAdapter sda = new SqlCeDataAdapter("select * from MyDemoTable", con);
+            sda.Fill(_ds);
+
+            if (_ds.Tables.Count > 0)
+            {
+                dgvOldData.DataSource = _ds.Tables[0];
+            }
+        }
+        /// <summary>
+        /// Reset All control of Form.
+        /// </summary>
+        private void Reset()
+        {
+            txtName.Text = string.Empty;
+            txtAge.Text = string.Empty;
+            txtQualification.Text = string.Empty;
+            btnDelete.Enabled = false;
+            brnUpdate.Enabled = false;
+            btnAdd.Enabled = true;
+        }
+
+      
+    }
+}
